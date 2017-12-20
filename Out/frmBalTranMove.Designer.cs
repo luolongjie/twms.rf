0bfa574d442f982a161b047402c44aaa369c9cb7
yangyang <yy_zolo@163.com>
2017-12-20 10:22:53 +0800

    no message

---
+++ Out/frmBalTranMove.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Rf_Wms.Out
+{
+    partial class frmBalTranMove
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnkeyboard = new System.Windows.Forms.Button();
+            this.labtoslname = new System.Windows.Forms.Label();
+            this.txttotraycode = new System.Windows.Forms.TextBox();
+            this.btnData = new System.Windows.Forms.Button();
+            this.txttoslid = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.labtrayqty = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labfromsIId = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtminqty = new System.Windows.Forms.TextBox();
+            this.labminunit = new System.Windows.Forms.Label();
+            this.labcommonUnit = new System.Windows.Forms.Label();
+            this.txtcommonqty = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.labneedqty = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbarcode = new System.Windows.Forms.TextBox();
+            this.txttoslname = new System.Windows.Forms.TextBox();
+            this.cmbtoslname = new System.Windows.Forms.ComboBox();
+            this.labccode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnkeyboard
+            // 
+            this.btnkeyboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(154)))), ((int)(((byte)(224)))));
+            this.btnkeyboard.ForeColor = System.Drawing.Color.White;
+            this.btnkeyboard.Location = new System.Drawing.Point(7, 271);
+            this.btnkeyboard.Name = "btnkeyboard";
+            this.btnkeyboard.Size = new System.Drawing.Size(72, 20);
+            this.btnkeyboard.TabIndex = 320;
+            this.btnkeyboard.Text = "键盘";
+            // 
+            // labtoslname
+            // 
+            this.labtoslname.Location = new System.Drawing.Point(173, 249);
+            this.labtoslname.Name = "labtoslname";
+            this.labtoslname.Size = new System.Drawing.Size(62, 20);
+            // 
+            // txttotraycode
+            // 
+            this.txttotraycode.Location = new System.Drawing.Point(84, 220);
+            this.txttotraycode.Name = "txttotraycode";
+            this.txttotraycode.Size = new System.Drawing.Size(140, 23);
+            this.txttotraycode.TabIndex = 318;
+            // 
+            // btnData
+            // 
+            this.btnData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(156)))), ((int)(((byte)(18)))));
+            this.btnData.ForeColor = System.Drawing.Color.White;
+            this.btnData.Location = new System.Drawing.Point(83, 271);
+            this.btnData.Name = "btnData";
+            this.btnData.Size = new System.Drawing.Size(72, 20);
+            this.btnData.TabIndex = 317;
+            this.btnData.Text = "列表";
+            // 
+            // txttoslid
+            // 
+            this.txttoslid.Location = new System.Drawing.Point(84, 247);
+            this.txttoslid.Name = "txttoslid";
+            this.txttoslid.Size = new System.Drawing.Size(83, 23);
+            this.txttoslid.TabIndex = 316;
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(1, 222);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(77, 20);
+            this.label14.Text = "移入托盘";
+            // 
+            // labtrayqty
+            // 
+            this.labtrayqty.Location = new System.Drawing.Point(84, 172);
+            this.labtrayqty.Name = "labtrayqty";
+            this.labtrayqty.Size = new System.Drawing.Size(118, 20);
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(1, 172);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 20);
+            this.label10.Text = "待转储量";
+            // 
+            // labfromsIId
+            // 
+            this.labfromsIId.Location = new System.Drawing.Point(86, 34);
+            this.labfromsIId.Name = "labfromsIId";
+            this.labfromsIId.Size = new System.Drawing.Size(103, 20);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(1, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.Text = "暂存区";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(171)))), ((int)(((byte)(110)))));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(159, 271);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(72, 20);
+            this.btnExit.TabIndex = 314;
+            this.btnExit.Text = "返回";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtminqty
+            // 
+            this.txtminqty.Location = new System.Drawing.Point(131, 194);
+            this.txtminqty.Name = "txtminqty";
+            this.txtminqty.Size = new System.Drawing.Size(47, 23);
+            this.txtminqty.TabIndex = 313;
+            // 
+            // labminunit
+            // 
+            this.labminunit.Location = new System.Drawing.Point(180, 197);
+            this.labminunit.Name = "labminunit";
+            this.labminunit.Size = new System.Drawing.Size(44, 20);
+            this.labminunit.Text = "单位2";
+            // 
+            // labcommonUnit
+            // 
+            this.labcommonUnit.Location = new System.Drawing.Point(92, 197);
+            this.labcommonUnit.Name = "labcommonUnit";
+            this.labcommonUnit.Size = new System.Drawing.Size(45, 20);
+            this.labcommonUnit.Text = "单位1";
+            // 
+            // txtcommonqty
+            // 
+            this.txtcommonqty.Location = new System.Drawing.Point(40, 194);
+            this.txtcommonqty.Name = "txtcommonqty";
+            this.txtcommonqty.Size = new System.Drawing.Size(47, 23);
+            this.txtcommonqty.TabIndex = 312;
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(1, 197);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 20);
+            this.label13.Text = "数量";
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(1, 247);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(77, 20);
+            this.label12.Text = "移入库位";
+            // 
+            // labneedqty
+            // 
+            this.labneedqty.Location = new System.Drawing.Point(0, 98);
+            this.labneedqty.Name = "labneedqty";
+            this.labneedqty.Size = new System.Drawing.Size(238, 20);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.Text = "转储单号";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(1, 61);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 20);
+            this.label11.Text = "商品码";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(91, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 23);
+            this.textBox2.TabIndex = 340;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(1, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.Text = "批次";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(91, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 23);
+            this.textBox3.TabIndex = 343;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(1, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 20);
+            this.label4.Text = "生产日期";
+            // 
+            // txtbarcode
+            // 
+            this.txtbarcode.Location = new System.Drawing.Point(84, 59);
+            this.txtbarcode.Name = "txtbarcode";
+            this.txtbarcode.Size = new System.Drawing.Size(140, 23);
+            this.txtbarcode.TabIndex = 345;
+            this.txtbarcode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtbarcode_KeyPress);
+            // 
+            // txttoslname
+            // 
+            this.txttoslname.Location = new System.Drawing.Point(70, 31);
+            this.txttoslname.Name = "txttoslname";
+            this.txttoslname.Size = new System.Drawing.Size(80, 23);
+            this.txttoslname.TabIndex = 347;
+            this.txttoslname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txttoslname_KeyPress);
+            // 
+            // cmbtoslname
+            // 
+            this.cmbtoslname.Location = new System.Drawing.Point(156, 31);
+            this.cmbtoslname.Name = "cmbtoslname";
+            this.cmbtoslname.Size = new System.Drawing.Size(78, 23);
+            this.cmbtoslname.TabIndex = 346;
+            this.cmbtoslname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbtoslname_KeyPress);
+            // 
+            // labccode
+            // 
+            this.labccode.Location = new System.Drawing.Point(73, 4);
+            this.labccode.Name = "labccode";
+            this.labccode.Size = new System.Drawing.Size(151, 20);
+            // 
+            // frmBalTranMove
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(233)))), ((int)(((byte)(253)))));
+            this.ClientSize = new System.Drawing.Size(238, 295);
+            this.Controls.Add(this.labccode);
+            this.Controls.Add(this.txttoslname);
+            this.Controls.Add(this.cmbtoslname);
+            this.Controls.Add(this.txtbarcode);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnkeyboard);
+            this.Controls.Add(this.labtoslname);
+            this.Controls.Add(this.txttotraycode);
+            this.Controls.Add(this.btnData);
+            this.Controls.Add(this.txttoslid);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.labtrayqty);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.labfromsIId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtminqty);
+            this.Controls.Add(this.labminunit);
+            this.Controls.Add(this.labcommonUnit);
+            this.Controls.Add(this.txtcommonqty);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.labneedqty);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label11);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmBalTranMove";
+            this.Text = "结余转储";
+            this.Load += new System.EventHandler(this.frmBalTranMove_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnkeyboard;
+        private System.Windows.Forms.Label labtoslname;
+        private System.Windows.Forms.TextBox txttotraycode;
+        private System.Windows.Forms.Button btnData;
+        private System.Windows.Forms.TextBox txttoslid;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label labtrayqty;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labfromsIId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtminqty;
+        private System.Windows.Forms.Label labminunit;
+        private System.Windows.Forms.Label labcommonUnit;
+        private System.Windows.Forms.TextBox txtcommonqty;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label labneedqty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbarcode;
+        private System.Windows.Forms.TextBox txttoslname;
+        private System.Windows.Forms.ComboBox cmbtoslname;
+        private System.Windows.Forms.Label labccode;
+    }
+}
---
+++ Out/frmBalTranMove.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Rf_Wms.Out
+{
+    public partial class frmBalTranMove : Form
+    {
+        public frmBalTranMove()
+        {
+            InitializeComponent();
+        }
+
+        Model.MZcqSlList mz = null;
+        Model.Mmsg msg = null;
+        private void frmBalTranMove_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                string x = HttpHelper.HttpPost("getZcqSlList", "&lcCode=" + Comm.lcCode + "&whId=" + Comm.warehousecode);
+                msg = (Model.Mmsg)JsonConvert.DeserializeObject(x, typeof(Model.Mmsg));
+                if (msg == null)
+                    throw new Exception("错误信息捕捉失败");
+                if (!msg.success)
+                    throw new Exception(msg.msg);
+                mz = (Model.MZcqSlList)JsonConvert.DeserializeObject(x, typeof(Model.MZcqSlList));
+                this.cmbtoslname.DataSource = mz.data;
+                this.cmbtoslname.ValueMember = "slId";
+                this.cmbtoslname.DisplayMember = "slName";
+
+                x = HttpHelper.HttpPost("getBalanceTransferOrderNumber", "&lcCode=" + Comm.lcCode + "&whId=" + Comm.warehousecode + "&updater=" + Comm.usercode);
+                msg = (Model.Mmsg)JsonConvert.DeserializeObject(x, typeof(Model.Mmsg));
+                if (msg == null)
+                    throw new Exception("错误信息捕捉失败");
+                if (!msg.success)
+                    throw new Exception(msg.msg);
+                Model.Mccode mc=  (Model.Mccode)JsonConvert.DeserializeObject(x, typeof(Model.Mccode));
+                this.labccode.Text = mc.data;
+                Cursor.Current = Cursors.Default;
+            }
+            catch (Exception ex)
+            {
+                Cursor.Current = Cursors.Default;
+                MessageBox.Show(ex.Message);
+                
+                return;
+            }
+        }
+
+        private void txttoslname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 13)
+                return;
+            if (this.txttoslname.Text != "")
+            {
+                var v = from x in mz.data where x.slName == this.txttoslname.Text select x;
+                if (v.Count() == 0)
+                {
+                    MessageBox.Show("没有该暂存区");
+                    this.txttoslname.SelectAll();
+                    return;
+                }
+                else
+                {
+                    this.cmbtoslname.Text = this.txttoslname.Text;
+                }
+            }
+            cmbtoslname_KeyPress(null, new KeyPressEventArgs((char)Keys.Enter));
+        }
+
+        private void cmbtoslname_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != 13)
+                return;
+            this.cmbtoslname.Enabled = false;
+            this.txttoslname.Enabled = false;
+            this.txtbarcode.Enabled = true;
+            this.txtbarcode.Focus();
+        }
+
+        private void txtbarcode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)
+            {
+                this.txtbarcode.Text = "";
+                this.txtbarcode.Enabled = false;
+                this.cmbtoslname.Enabled = true;
+                this.txttoslname.Enabled = true;
+                this.txttoslname.Text = "";
+                this.txttoslname.Focus();
+            }
+            Model.Mmaterialcode m = null;
+            if (e.KeyChar != 13)
+                return;
+            try
+            {
+                Cursor.Current = Cursors.WaitCursor;
+                string x = HttpHelper.HttpPost("getMaterialCode", @"barCode=" + this.txtbarcode.Text + "&lcCode=" + Comm.lcCode );
+                msg = (Model.Mmsg)JsonConvert.DeserializeObject(x, typeof(Model.Mmsg));
+                if (msg == null)
+                    throw new Exception("错误信息捕捉失败");
+                if (!msg.success)
+                    throw new Exception(msg.msg);
+                m = (Model.Mmaterialcode)JsonConvert.DeserializeObject(x, typeof(Model.Mmaterialcode));
+                if (m == null)
+                {
+                    throw new Exception("数据信息捕捉失败");
+                }
+                if (m.data.Count == 0 || m.data[0].code == null)
+                {
+                    throw new Exception("没有该条码对应的数据");
+                }
+                Cursor.Current = Cursors.Default;
+            }
+            catch (Exception ex)
+            {
+                Cursor.Current = Cursors.Default;
+                this.txtbarcode.Text = "";
+                MessageBox.Show(ex.Message);
+                return;
+
+            }
+            Model.Mmaterialcodebody materialbody = null;
+            if (m.data.Count > 1)
+            {
+                Ot.frmMaterial frm = new Rf_Wms.Ot.frmMaterial();
+                frm.m = m;
+                frm.ShowDialog();
+                if (frm.mbody != null)
+                    materialbody = frm.mbody;
+            }
+            else
+            {
+                materialbody = m.data[0];
+            }
+            if (materialbody != null)
+            {
+                try
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    string x = HttpHelper.HttpPost("getlocationStockList", @"&whId=" + Comm.warehousecode + "&lcCode=" + Comm.lcCode + "&materialCode=" + materialbody.code + "&slId=" + this.cmbtoslname.SelectedValue.ToString());
+                    msg = (Model.Mmsg)JsonConvert.DeserializeObject(x, typeof(Model.Mmsg));
+                    if (msg == null)
+                        throw new Exception("错误信息捕捉失败");
+                    if (!msg.success)
+                        throw new Exception(msg.msg);
+                    m = (Model.Mmaterialcode)JsonConvert.DeserializeObject(x, typeof(Model.Mmaterialcode));
+                    if (m == null)
+                    {
+                        throw new Exception("数据信息捕捉失败");
+                    }
+                   
+                    Cursor.Current = Cursors.Default;
+                }
+                catch (Exception ex)
+                {
+                    Cursor.Current = Cursors.Default;
+                    this.txtbarcode.Text = "";
+                    MessageBox.Show(ex.Message);
+                    return;
+
+                }
+               
+
+            }
+            else
+            {
+                this.txtbarcode.SelectAll();
+                return;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+
+      
+    }
+}
